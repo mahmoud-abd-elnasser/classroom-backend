d7c368817a5929215b75c13cd8c5070b21e12544
mahmoud-abd-elnasser <mahmoudabdelnaser248@gmail.com>
2026-04-06 18:19:47 +0200

    implement better auth

--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,8 @@ import express from 'express';
 import cors from 'cors';
 import subjectsRouter from "./routes/subjects";
 import securityMiddleware from "./middleware/security";
+import { toNodeHandler } from "better-auth/node";
+import { auth } from "./lib/auth";
 
 const app = express();
 const port = 8000;
@@ -24,6 +26,8 @@ app.use(cors({
   methods: ['GET', 'POST', 'PUT', 'DELETE'],
   credentials: true
 }));
+
+app.all('/api/auth/*splat', toNodeHandler(auth));
 
 app.use(express.json());
 
