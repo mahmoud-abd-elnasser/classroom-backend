ef62ff32ae86f48af863f07816a7b93edd9d87bc
mahmoud-abd-elnasser <mahmoudabdelnaser248@gmail.com>
2026-04-08 14:10:10 +0200

    fix tsconfig deprecated moduleResolution

--- src/index.ts
+++ src/index.ts
@@ -16,20 +16,20 @@ const port = 8000;
 const frontendUrl = process.env.FRONTEND_URL;
 if (!frontendUrl) {
     throw new Error('FRONTEND_URL is not defined');
-  }
+}
 
 const allowedOrigins = [frontendUrl, frontendUrl.endsWith('/') ? frontendUrl.slice(0, -1) : `${frontendUrl}/`];
 
 app.use(cors({
-  origin: (origin, callback) => {
-    if (!origin || allowedOrigins.includes(origin)) {
-      callback(null, true);
-    } else {
-      callback(null, false);
-    }
-  },
-  methods: ['GET', 'POST', 'PUT', 'DELETE'],
-  credentials: true
+    origin: (origin, callback) => {
+        if (!origin || allowedOrigins.includes(origin)) {
+            callback(null, true);
+        } else {
+            callback(null, false);
+        }
+    },
+    methods: ['GET', 'POST', 'PUT', 'DELETE'],
+    credentials: true
 }));
 
 app.use(express.json());
@@ -41,9 +41,9 @@ app.all('/api/auth/*splat', toNodeHandler(auth));
 app.use('/api/subjects', subjectsRouter);
 
 app.get('/', (req, res) => {
-  res.json({ message: 'Hello from Classroom Backend!' });
+    res.json({ message: 'Hello from Classroom Backend!' });
 });
 
 app.listen(port, () => {
-  console.log(`Server is running at http://localhost:${port}`);
+    console.log(`Server is running at http://localhost:${port}`);
 });
